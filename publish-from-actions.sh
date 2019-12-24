f270470a3f3325f6308d695abfcf9da9baad9f85
Kasra Bigdeli <kasra85@gmail.com>
2019-12-24 16:43:17 -0500

    updated

--- publish-from-actions.sh
+++ publish-from-actions.sh
@@ -43,6 +43,7 @@ if [[ "$REPONAME" == "$GHIO" ]]; then
 else
   REMOTE_BRANCH="gh-pages"
 fi && \
+REMOTE_BRANCH="test-publish-pages"
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
