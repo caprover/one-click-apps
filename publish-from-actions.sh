3546455cc1fcf0a19165fa0916493011b56d1271
Kasra Bigdeli <kasra85@gmail.com>
2019-12-24 17:44:47 -0500

    Fixed branch issue

--- publish-from-actions.sh
+++ publish-from-actions.sh
@@ -88,4 +88,4 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo `date` >> forcebuild.date
 git add -A 
 git commit -m 'Deploy to GitHub Pages' 
-git push --force $REMOTE_REPO master:$REMOTE_BRANCH 
+git push $REMOTE_REPO $REMOTE_BRANCH:$REMOTE_BRANCH 
