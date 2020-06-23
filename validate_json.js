51c113d539b196205e23b51da91ad189a59fa992
Simon Belbeoch <simon.belbeoch@gmail.com>
2020-06-23 22:13:31 +0200

    fix ident + make a const for display messages

--- validate_json.js
+++ validate_json.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+// Requirements
 const path = require('path')
 const fs = require('fs-extra')
 
@@ -10,50 +11,59 @@ const ONE_APP_EXTENSION = '.json'
 const APP_FOLDER = 'apps'
 const LOGO_FOLDER = 'logos'
 
+// VALIDATION functions
 const isLogoValid = ({ existsSync, statSync }, logoFullPath) => existsSync(logoFullPath) && statSync(logoFullPath).isFile()
 const isVersionValid = (applicationVersion, capVersion) => applicationVersion === capVersion
-const versionErrorDisplay = (appVersion, capVersion, filename) => `unmatched versions ${appVersion} ${capVersion} for ${filename}`
-const logoErrorDisplay = (filename, logoFullPath) =>`Cannot find logo for ${filename} ${printablePath(logoFullPath)}`
+
+// Displayed Messages
 const printablePath = fullPath => fullPath.substr(fullPath.indexOf(`/${PUBLIC}`))
+const versionErrorMessage = (appVersion, capVersion, filename) => `unmatched versions ${appVersion} ${capVersion} for ${filename}`
+const logoErrorMessage = (filename, logoFullPath) =>`Cannot find logo for ${filename} ${printablePath(logoFullPath)}`
 const defaultDisplayName = appName => appName.charAt(0).toUpperCase() + appName.slice(1)
-const endDisplay = appNumber => `${appNumber} applications validated.`
+const endMessage = appNumber => `${appNumber} applications validated.`
+const validatedMessage = filename => `Validated ${filename}`
+
+const DISPLAY = {
+  ERROR: {
+    LOGO: logoErrorMessage,
+    VERSION: versionErrorMessage,
+  },
+  LOG: {
+    END: endMessage,
+    VALIDATED: validatedMessage,
+  },
+}
 
  // validating version 2
  async function validate() {
-     const pathOfVersion = path.join(pathOfPublic, 'v' + VERSION)
-     const pathOfApps = path.join(pathOfVersion, APP_FOLDER)
-     const versionString = VERSION + ''
-     const files = await fs.readdir(pathOfApps)
-     const apps = files.filter(fileName => fileName.includes(ONE_APP_EXTENSION))
+  const pathOfVersion = path.join(pathOfPublic, 'v' + VERSION)
+  const pathOfApps = path.join(pathOfVersion, APP_FOLDER)
+  const versionString = VERSION + ''
+  const files = await fs.readdir(pathOfApps)
+  const apps = files.filter(fileName => fileName.includes(ONE_APP_EXTENSION))
 
-     const applicationsDetails = apps.map(filename => {
-      const contentString = fs.readFileSync(path.join(pathOfApps, filename))
-      const appName = filename.replace(ONE_APP_EXTENSION, '')
-      const {
-        captainVersion: appVersion,
-        displayName = defaultDisplayName(appName),
-        description = ''
-      } = JSON.parse(contentString)
-      if (!isVersionValid(appVersion, versionString)) {
-        throw new Error(versionErrorDisplay(appVersion, versionString, filename))
-      }
-      const logoFileName = appName + LOGO_EXTENSION
-      const logoFullPath = path.join(pathOfVersion, LOGO_FOLDER, logoFileName)
+  const applicationsDetails = apps.map(filename => {
+    const contentString = fs.readFileSync(path.join(pathOfApps, filename))
+    const appName = filename.replace(ONE_APP_EXTENSION, '')
+    const {
+      captainVersion: appVersion,
+      displayName = defaultDisplayName(appName),
+      description = '',
+    } = JSON.parse(contentString)
+    if (!isVersionValid(appVersion, versionString)) {
+      throw new Error(DISPLAY.ERROR.VERSION(appVersion, versionString, filename))
+    }
 
-       if (!isLogoValid(fs, logoFullPath)) {
-         throw new Error(logoErrorDisplay(filename, logoFullPath))
-       }
-       console.log(`Validated ${filename}`)
-        return {
-          name: appName,
-          displayName,
-          description,
-          logoUrl: logoFileName,
-          logoFullPath
-        }
-      })
-    console.log(endDisplay(applicationsDetails.length))
- }
+    const logoFileName = appName + LOGO_EXTENSION
+    const logoFullPath = path.join(pathOfVersion, LOGO_FOLDER, logoFileName)
+    if (!isLogoValid(fs, logoFullPath)) {
+      throw new Error(DISPLAY.ERROR.LOGO(filename, logoFullPath))
+    }
+    console.log(DISPLAY.LOG.VALIDATED(filename))
+    return { name: appName, displayName, description, logoUrl: logoFileName, logoFullPath }
+  })
+  console.log(DISPLAY.LOG.END(applicationsDetails.length))
+}
 
 try {
   return validate()
