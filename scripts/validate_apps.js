e8cff0e27e48ff4cc0b988b3d97a1135b9de2c1d
Simon Belbeoch <simon.belbeoch@ln1.eu>
2024-11-11 17:12:06 +0100

    remove dead code

--- scripts/validate_apps.js
+++ scripts/validate_apps.js
@@ -15,11 +15,11 @@
      const pathOfApps = path.join(pathOfVersion, 'apps');
 
      return fs.readdir(pathOfApps)
-         .then(function (items) {
+         .then(function (files) {
 
-             const apps = items.filter(v => v.includes('.yml'));
+             const apps = files.filter(v => v.includes('.yml'));
 
-             if (items.length !== apps.length) {
+             if (files.length !== apps.length) {
                  throw new Error('All files in v4 must end with .yml');
              }
 
@@ -95,65 +95,7 @@
          });
  }
 
- // validating version 2
- function validateV2() {
-
-     const version = '2';
-     const pathOfVersion = path.join(pathOfPublic, 'v' + version);
-     const pathOfApps = path.join(pathOfVersion, 'apps');
-
-     if (!fs.existsSync(pathOfApps)) {
-         return;
-     }
-
-     return fs.readdir(pathOfApps)
-         .then(function (items) {
-
-             const apps = items.filter(v => v.includes('.json'));
-
-             if (items.length !== apps.length) {
-                 throw new Error('All files in v2 must end with .json');
-             }
-
-             for (var i = 0; i < apps.length; i++) {
-                 const contentString = fs.readFileSync(path.join(pathOfApps, apps[i]));
-                 const content = JSON.parse(contentString);
-                 const captainVersion = (content.captainVersion + '');
-                 const versionString = (version + '');
-                 if (versionString !== captainVersion)
-                     throw new Error(`unmatched versions   ${versionString}  ${captainVersion} for ${apps[i]}`);
-
-                 apps[i] = apps[i].replace('.json', '');
-
-                 if (!content.description) {
-                     throw new Error(`Cannot find description for ${apps[i]}`);
-                 }
-                 if (content.description.length > 200) {
-                     throw new Error(`Description too long for ${apps[i]}  - keep it below 200 chars`);
-                 }
-
-                 const logoFileName = apps[i] + '.png';
-
-                 const logoFullPath = path.join(pathOfVersion, 'logos', logoFileName);
-
-                 if (!fs.existsSync(logoFullPath) ||
-                     !fs.statSync(logoFullPath).isFile()) {
-                     let printablePath = logoFullPath;
-                     printablePath = printablePath.substr(printablePath.indexOf(`/${PUBLIC}`));
-                     throw new Error(`Cannot find logo for ${apps[i]} ${printablePath}`);
-                 }
-
-                 console.log(`Validated ${apps[i]}`);
-
-             }
-
-         });
- }
-
  Promise.resolve()
-     .then(function () {
-         return validateV2();
-     })
      .then(function () {
          return validateV4();
      })
